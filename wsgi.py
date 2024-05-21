ad9c9ac8cdbe52c5bb442a5bc83b3235516a183d
martinezabraham194 <35661461+martinezabraham194@users.noreply.github.com>
2024-05-21 15:20:49 -0400

    fixed redis connection

--- wsgi.py
+++ wsgi.py
@@ -2,6 +2,7 @@ import os
 from app import create_app
 from app.celery_utils import make_celery
 from redis import Redis
+from flask import url_for
 
 # Get the configuration name from the environment variable FLASK_CONFIG
 # Default to 'production' if the variable is not set
@@ -11,15 +12,10 @@ celery = make_celery(app)  # Initialize Celery after creating the Flask app
 
 @app.route('/')
 def home():
-    welcome_message = """Hey if you want to test your redis connection: 
-    Quick Test: go to /test_redis 
-    Detailed Test: go to /test_redis_connection"""
+    redis_test_url = url_for('test_redis_connection')
+    welcome_message = f"""Hey if you want to test your redis connection: 
+    Detailed Test: <a href="{redis_test_url}">go to /test_redis_connection</a>"""
     return welcome_message
-
-@app.route('/test_redis')
-def test_redis():
-    result = celery.send_task('test_redis_connection')
-    return str(result.get())
 
 @app.route('/test_redis_connection')
 def test_redis_connection():
