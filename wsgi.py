96c6fe9d69dd0f126d5dc3542fe096e2e0853adf
distortion2112 <38536863+distortion2112@users.noreply.github.com>
2024-05-20 23:07:19 -0500

    break apart app instantiation

--- wsgi.py
+++ wsgi.py
@@ -4,12 +4,21 @@ from app import create_app
 # Get the configuration name from the environment variable FLASK_CONFIG
 # Default to 'production' if the variable is not set
 config_name = os.getenv('FLASK_CONFIG', 'production')
+app = create_app(config_name)  # Create the Flask application using the specified configuration
 
-# Create the Flask application using the specified configuration
-app, celery = create_app(config_name)  # Create both app and celery
+celery = make_celery(app)  # Initialize Celery after creating the Flask app
 
 # If this script is executed directly, run the Flask development server
 # Note: This block is not used when deploying with Gunicorn
 if __name__ == "__main__":
     app.run()
-    
+
+
+    import os
+from app import create_app
+from app.celery_utils import make_celery
+
+
+
+if __name__ == "__main__":
+    app.run()
